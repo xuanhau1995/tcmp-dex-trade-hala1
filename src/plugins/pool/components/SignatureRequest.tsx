638e3738797b3be4ff3a66f39bd0d37100774651
haula-dv <hau.la.dv@gmail.com>
2024-08-18 20:28:20 +0700

    a

--- src/plugins/pool/components/SignatureRequest.tsx
+++
@@ -1,5 +0,0 @@
-import { MainCard } from "@/components/card/MainCard";
-
-export const SignatureRequest = () => {
-  return <MainCard>SignatureRequest</MainCard>;
-};
--- src/plugins/swap/type.ts
+++
@@ -1,9 +0,0 @@
-export interface IExchangePrice {
-  chain: string;
-  account: string;
-  inTokenAddress: string;
-  outTokenAddress: string;
-  isExactIn: boolean;
-  slippage: number;
-  inTokenAmount: string;
-}
