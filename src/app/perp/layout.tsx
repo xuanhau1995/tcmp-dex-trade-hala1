0a6f89f4505918ef67652d5c71be680e5874fd0c
haula-dv <hau.la.dv@gmail.com>
2024-08-18 20:38:09 +0700

    a

--- src/app/perp/layout.tsx
+++ src/app/perp/layout.tsx
@@ -1,6 +1,5 @@
 'use client';
 import { Header } from '@/components/layouts/Header';
-import { UnSupportNextworkAlert } from '@/plugins/wallet-old/components/UnSupportNextworkAlert';
 import { OrderlyConfigProviderRoot } from '@/provider/OrderlyConfigProviderRoot';
 import Web3OnboardProviderRoot from '@/provider/WalletConnectProvider';
 import { Box } from '@mui/material';
@@ -16,7 +15,6 @@ export default function RootLayout({
 			<Web3OnboardProviderRoot>
 				<OrderlyConfigProviderRoot>
 					<Header />
-					<UnSupportNextworkAlert />
 
 					<Box p={2}>{children}</Box>
 				</OrderlyConfigProviderRoot>
